5465d697ba90cda66b2060a798ca23d24861edbc
Katlyn Boches <86095070+Katlyn627@users.noreply.github.com>
2022-09-07 12:18:35 -0600

    styling problems

--- ChefCode Problems/JavaScript/ticketfine.js
+++ ChefCode Problems/JavaScript/ticketfine.js
@@ -1,26 +1,31 @@
 // Problem
-// On a certain train, Chef-the ticket collector, collects a fine of Rs. XX if a passenger is travelling without a ticket. It is known that a passenger carries either a single ticket or no ticket.
 
+// On a certain train, Chef-the ticket collector, collects a fine of Rs. XX if a passenger is travelling without a ticket. It is known that a passenger carries either a single ticket or no ticket.
 // PP passengers are travelling and they have a total of QQ tickets. Help Chef calculate the total fine collected.
 
-// Input Format
+// Input Format:
+
+
 // The first line contains a single integer TT, the number of test cases. TT lines follow. Each following line contains three integers separated by spaces, whose description follows.
+            // The first integer, XX, is the fee in rupees.
+            // The second integer, PP, is the number of passengers on the train.
+            // The third integer, QQ, is the number of tickets Chef collected.
 
-// The first integer, XX, is the fee in rupees.
-// The second integer, PP, is the number of passengers on the train.
-// The third integer, QQ, is the number of tickets Chef collected.
-// Output Format
+
+// Output Format:
+
+
 // The output must consist of TT lines.
+            // The i^{th}ith line must contain a single integer, the total money(in rupees) collected by Chef corresponding to the i^{th}ith test case.
 
-// The i^{th}i 
-// th
-//   line must contain a single integer, the total money(in rupees) collected by Chef corresponding to the i^{th}i 
-// th
-//   test case.
+
 // Constraints
+
 // 1 \le T \le 101≤T≤10
 // 1 \le X \le 101≤X≤10
 // 0 \le Q \le P \le 100≤Q≤P≤10
+
+
 // Sample 1:
 // Input
 // Output
@@ -33,6 +38,7 @@
 // 6
 // 8
 // 63
+
 // Explanation:
 // Test case 11: The total number of passengers travelling without ticket are 1 - 1 = 01−1=0. So the total fine collected is 0 \cdot 4 = 00⋅4=0 rupees.
 
